23751155cbc10e235b74b6c7fb4c62058178b71f
makerseok <maker7788@gmail.com>
2022-11-01 18:54:06 +0900

    add https cert

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import http from 'http';
+import https from 'https';
 import socketio from 'socket.io';
 import cors from 'cors';
+import { readFileSync } from 'fs';
+
+import conf from './config.json';
+
+const key = readFileSync(conf.KEY_PATH);
+const cert = readFileSync(conf.CERT_PATH);
 
 const app = express();
 
@@ -11,13 +18,14 @@ app.get('/', (_req: Request, res: Response) => {
   res.send({ uptime: process.uptime() });
 });
 
-const server = http.createServer(app);
-const io = new socketio.Server(server);
+const httpsServer = https.createServer({ key, cert }, app);
+
+const io = new socketio.Server(httpsServer);
 
 io.on('connection', (...params) => {
   console.log(params);
 });
 
-server.listen(3000, () => {
+httpsServer.listen(3000, () => {
   console.log('Running at localhost:3000');
 });
